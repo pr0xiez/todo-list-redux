104dbb38ad19dfa2a02dc93339b73ba4f23c6a8c
Alex Hall <alex.hallll.03@gmail.com>
2018-03-06 16:56:00 -0500

    root reducer and redux setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension'
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import React from 'react';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
+  <Provider store={store}>
     <App />
+  </Provider>
   , document.getElementById('root')
 );
-registerServiceWorker();
